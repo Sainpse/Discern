10d1e26b4c7cab48fe3a53c4207b2d3dce1cc85b
unknown <marcusmadumo.k@live.com>
2018-06-24 16:03:41 +0200

    Artificial Neural Network From Scratch For Digits Classification

---
+++ ANN/activations.py
@@ -0,0 +1,28 @@
+import numpy as np 
+
+
+"""________________________________________ A C T I V A T I O N   F U N C T I O N S ______________________________________________
+    Activation functions
+    relu    -  Generally better than sigmoid but has a problem of losing or killing neurons
+    sigmoid -  Was the commonly used
+    softmax -  Better for mutli classification and it is stabilized by normalizing the values to avoid nan
+    _______________________________________________________________________________________________________________________________
+"""
+def sigmoid(Z):
+    activation = 1.0/(1.0+np.exp(-Z))
+    return activation, Z
+
+def sigmoid_plain(Z):
+    activation = 1.0/(1.0+np.exp(-Z))
+    return activation
+
+def relu(Z):
+    #return the maximum between two vectors
+    activation = np.maximum(Z, 0)
+    return activation, Z
+
+def softmax(Z):
+    #axis=0 sum along rows
+    exp = np.exp(Z - np.max(Z))
+    activation = exp/(np.sum(exp,axis=0))
+    return activation, Z
